a8a875393f654b9b3b0157feb7c858a57de1e2f3
Raphaelj <raphaeljames897@gmail.com>
2025-03-26 15:18:41 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<p className="text-center text-red-500">Click on the Vite and React logos to learn more</p>
+		</>
+	);
+}
+
+export default App;
