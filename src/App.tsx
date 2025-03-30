c66005ef63f92c09b3dc74b56009100a68295ea7
Raphaelj <raphaeljames897@gmail.com>
2025-03-30 16:55:33 +0100

    Screen height

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Homepage from './pages/Homepage';
 
 function App() {
 	return (
-		<div className="sm:bg-gray-100 flex max-h-screen">
+		<div className="sm:bg-gray-100 flex h-screen">
 			<Homepage />
 		</div>
 	);
