5939cfc308f45b0bf7032c8c8248adaa224b8a24
Raphaelj <raphaeljames897@gmail.com>
2025-03-30 20:56:34 +0100

    User friendly welcome message

--- src/components/Greeeting.tsx
+++ src/components/Greeeting.tsx
@@ -1,10 +1,24 @@
+function getWelcomeMessage(): string {
+	const hour = new Date().getHours();
+
+	if (hour >= 5 && hour < 12) {
+		return 'Good Morning! ☀️'
+	} else if (hour >= 12 && hour < 17) {
+		return 'Hot Afternoon, Innit!'
+	} else if (hour >= 17 && hour <= 22) {
+		return 'Hey there!'
+	} else {
+		return 'Hi Friend!'
+	}
+}
+
 function Greeting() {
-	let user = 'Raph';
+	let greeting = getWelcomeMessage();
 
 	return (
 		<div className="text-center text-sm max-w-72 sm:max-w-84 mx-auto">
-			<h1 className="text-2xl sm:text-3xl font-medium mb-2">Good Morning, {user} 👋</h1>
-			<p className="text-xs sm:text-sm text-neutral-600">Choose a prompt below or write your own to start chatting with MultimediaAI</p>
+			<h1 className="text-2xl sm:text-3xl font-medium mb-2">{greeting}</h1>
+			<p className="text-xs sm:text-sm text-neutral-600">Pick a prompt below or share your own idea to start chatting with MultimediaAI</p>
 		</div>
 	);
 }
